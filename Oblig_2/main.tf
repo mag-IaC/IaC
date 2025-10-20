c2a40dd7048f70adc7a17e0936490f0bf738ce3d
mag-lange <magnolan@stud.ntnu.no>
2025-10-20 15:35:56 +0200

    c

--- Oblig_2/main.tf
+++ Oblig_2/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate-mag2412"  
-    storage_account_name = "sttfstatemag1234"     
+    resource_group_name  = "rg-tfstate-mag2412"
+    storage_account_name = "sttfstatemag1234"
     container_name       = "tfstate"
     key                  = "projects/oblig2/terraform.tfstate"
-    access_key = "KO131KipZ9o4p4q8a+pzogEC36XcbevEhvJoqtJBSYS6Rkz3LCL/3VcsBdP+dSUJBmVOierINBaa+AStiJPa8w==" #found with the command az storage account keys list `--resource-group rg-tfstate-mag2412  --account-name sttfstatemag2412 --query "[0].value" -o tsv
+    access_key           = "KO131KipZ9o4p4q8a+pzogEC36XcbevEhvJoqtJBSYS6Rkz3LCL/3VcsBdP+dSUJBmVOierINBaa+AStiJPa8w==" #found with the command az storage account keys list `--resource-group rg-tfstate-mag2412  --account-name sttfstatemag2412 --query "[0].value" -o tsv
 
   }
   required_providers {
@@ -42,11 +42,11 @@ resource "azurerm_resource_group" "rg" {
 module "storage_account" {
   source = "./modules/sa"
 
-  environment       = var.environment
-  location          = var.location
-  resource_group    = azurerm_resource_group.rg.name
-  project_name      = var.project_name
-  storage_tier      = var.storage_tier
-  replication_type  = var.replication_type
-  name              = local.storage_account
+  environment      = var.environment
+  location         = var.location
+  resource_group   = azurerm_resource_group.rg.name
+  project_name     = var.project_name
+  storage_tier     = var.storage_tier
+  replication_type = var.replication_type
+  name             = local.storage_account
 }
